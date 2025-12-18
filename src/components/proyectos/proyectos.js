3a68ca4a8d6cc1801b7d5b9f589337cb45d5a15f
Luci-fer666 <luciano.illuminati17@gmail.com>
2025-12-18 00:37:32 -0300

    Agrego de nuevos proyectos y cambias visuales

--- src/components/proyectos/proyectos.js
+++ src/components/proyectos/proyectos.js
@@ -4,40 +4,66 @@ const proyectos = [
     paginaWeb: "https://luci-fer666-hermanos-jota-sping-7-8-nu.vercel.app/productos",
     imagen: "/docs/proyectos/hermanos.jpg",
     git: "https://github.com/Luci-fer666/Hermanos-Jota-Sping-7-8",
-    descripcion: "Proyecto e-commerce desarrollado con stack MERN.",
-    tecnologias: ["MongoDB", "Express", "React", "Node"]
+    descripcion: "Proyecto e-commerce desarrollado de una muebleria",
+    tecnologias: ["2025-2025", "MongoDB", "Express", "React", "Node"]
   },
   {
     nombre: "GameHub Solidario",
     paginaWeb: "https://game-hub-solidario.vercel.app",
     imagen: "/docs/proyectos/gamehub.jpg",
     git: "https://github.com/Luci-fer666/GameHub-Solidario",
-    descripcion: "Plataforma solidaria orientada a videojuegos.",
-    tecnologias: ["HTML", "CSS", "JavaScript"]
+    descripcion: "Plataforma solidaria de torneos de videojuegos",
+    tecnologias: ["2025-2025", "HTML", "CSS", "JavaScript"]
   },
   {
     nombre: "CyberFest Festival 2025",
     paginaWeb: "https://cyberfest-lovat.vercel.app",
     imagen: "/docs/proyectos/cyberfest.jpg",
     git: "https://github.com/Luci-fer666/cyberfest",
-    descripcion: "Landing page para evento tecnológico.",
-    tecnologias: ["HTML", "CSS"]
+    descripcion: "Pagina para festival de musica electronica",
+    tecnologias: ["2025-2025", "HTML", "CSS"]
   },
   {
     nombre: "Philosophorum Page",
-    paginaWeb: "https://cyberfest-lovat.vercel.app",
-    imagen: "/docs/proyectos/gamehub.jpg",
-    git: "https://github.com/Luci-fer666/cyberfest",
-    descripcion: "Proyecto experimental. Actualmente no disponible.",
-    tecnologias: ["Flask", "Python", "HTML"]
+    paginaWeb: "/proyectos",
+    imagen: "/docs/proyectos/pylosophorum.jpg",
+    git: "/proyectos",
+    descripcion: "NO DISPONIBLE. Pagina estudiantil para realizar calculos quimicos",
+    tecnologias: ["2024-2026", "Flask", "Python", "HTML"]
   },
   {
     nombre: "Raiting Games",
-    paginaWeb: "https://cyberfest-lovat.vercel.app",
+    paginaWeb: "/proyectos",
     imagen: "/docs/proyectos/raiting.jpg",
-    git: "https://github.com/Luci-fer666/cyberfest",
-    descripcion: "Sistema de puntuación de videojuegos.",
-    tecnologias: ["PHP", "HTML", "CSS"]
+    git: "/proyectos",
+    descripcion: "NO DISPONIBLE. Pagina de puntuacion de videojuegos",
+    tecnologias: ["2024-2024", "PHP", "HTML", "CSS"]
+  },
+  {
+    nombre: "Luciano PortFolio",
+    paginaWeb: "https://mi-portfolio-black.vercel.app",
+    imagen: "/docs/proyectos/portfolio.jpg",
+    git: "https://github.com/Luci-fer666/miPortfolio",
+    descripcion: "Portfolio para presentar conocimientos y habilidades. Esta misma web",
+    tecnologias: ["2025-2025", "React", "Node"]
+  }
+  ,
+  {
+    nombre: "Lucifer ArtBook",
+    paginaWeb: "/proyectos",
+    imagen: "/docs/proyectos/portfolio.jpg",
+    git: "/proyectos",
+    descripcion: "Portfolio de ilustracion. Muestra ilustraciones digitales y diseños.",
+    tecnologias: ["2025-2025", "React", "Node"]
+  }
+  ,
+  {
+    nombre: "Grogu Tech Store",
+    paginaWeb: "/proyectos",
+    imagen: "/docs/proyectos/grogutech.jpg",
+    git: "/proyectos",
+    descripcion: "NO DISPONIBLE. Tienda digital de venta de videojuegos.",
+    tecnologias: ["2024-2026", "PHP", "HTML", "CSS"]
   }
 ];
 
